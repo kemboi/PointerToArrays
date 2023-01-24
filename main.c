d4460a89067f092e37666b8b529b54172bb52024
Evans Kemboi <evanskemb@gmail.com>
2023-01-24 09:37:57 +0000

    Declared a pointer and assigned it to arr[]

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  printf("A pointer to an array\n");
+  int arr = {1,6,7,10,5}; // array declaration
+  int *ptr=&arr; // pointer to an array
+  printf("This size of pointer to array is : %lu bytes", sizeof(*ptr));
+    
   return 0;
 }

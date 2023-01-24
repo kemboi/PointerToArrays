f428ca8fe77437d5052a0a818b6cd5e06cdf4d0b
Evans Kemboi <evanskemb@gmail.com>
2023-01-24 10:24:49 +0000

    Accessing array values using pointer

--- .ccls-cache/@home@runner@PointerToArrays/main.c
+++ .ccls-cache/@home@runner@PointerToArrays/main.c
@@ -2,9 +2,12 @@
 
 int main(void) {
   printf("A pointer to an array\n");
-  int arr = {1,6,7,10,5}; // array declaration
-  int *ptr=&arr; // pointer to an array
-  printf("This size of pointer to array is : %lu bytes", sizeof(*ptr));
-    
+  int arr = {1, 6, 7, 10, 5}; // array declaration
+  int *ptr = &arr;            // pointer to an array
+  printf("This size of pointer to array is : %lu bytes\n", sizeof(*ptr));
+  for (int i = 0; i < 5; i++) {
+    printf("Values %d :",
+           (*ptr + i)); // accessing values of array using pointers.
+  }
   return 0;
 }
